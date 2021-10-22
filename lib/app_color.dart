56c6abfd0f4acc9cd0066d599891db85b7f1a1fe
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-23 00:18:36 +0700

    add app color

---
+++ lib/app_color.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+
+  static final AppColor appColor = AppColor._();
+  // first green
+  static const Color firstGreen1 = Color(0xff5bba6f);
+  static const Color firstGreen2 = Color(0xff3fa34d);
+  static const Color firstGreen3 = Color(0xff2A9134);
+  static const Color firstGreen4 = Color(0xff137547);
+  static const Color firstGreen5 = Color(0xff054a29);
+
+  static const List<Color> firstGreenList = [
+    firstGreen1,
+    firstGreen2,
+    firstGreen3,
+    firstGreen4,
+    firstGreen5,
+  ];
+
+  // second green
+  static const Color secondGreen1 = Color(0xff95c5b2);
+  static const Color secondGreen2 = Color(0xff6db385);
+  static const Color secondGreen3 = Color(0xff427a51);
+  static const Color secondGreen4 = Color(0xff2a5434);
+  static const Color secondGreen5 = Color(0xff113b26);
+  static const Color secondGreen6 = Color(0xff222d26);
+
+  static const List<Color> secondGreenList = [
+    secondGreen1,
+    secondGreen2,
+    secondGreen3,
+    secondGreen4,
+    secondGreen5,
+    secondGreen6,
+  ];
+
+  // third green
+  static const Color thirdGreen1 = Color(0xff355a44);
+  static const Color thirdGreen2 = Color(0xff181e20);
+  static const Color thirdGreen3 = Color(0xff233835);
+  static const Color thirdGreen4 = Color(0xff4f7567);
+  static const Color thirdGreen5 = Color(0xff8eb2a2);
+  static const Color thirdGreen6 = Color(0xffc4d9cb);
+
+  static const List<Color> thirdGreenList = [
+    thirdGreen1,
+    thirdGreen2,
+    thirdGreen3,
+    thirdGreen4,
+    thirdGreen5,
+    thirdGreen6,
+  ];
+
+}
