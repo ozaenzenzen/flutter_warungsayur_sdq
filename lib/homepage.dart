e526af74fec89f1ab0982a1903c5185babdeef1e
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-24 00:34:35 +0700

    add homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_warungsayur_sdq/app_color.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({ Key? key }) : super(key: key);
@@ -11,7 +12,19 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      alignment: Alignment.center,
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      decoration: const BoxDecoration(
+        color: AppColor.firstGreen1,
+      ),
+      child: Column(
+        children: const [
+          Text("ABCBCBCBC"),
+          Text("ABCBCBCBC"),
+          Text("ABCBCBCBC"),
+        ],
+      ),
     );
   }
 }
